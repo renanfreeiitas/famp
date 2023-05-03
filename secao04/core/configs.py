601cb9dc82c85dd11a854217be6df0ad2b9ecaf4
renan <renanfreeiitas@gmail.com>
2023-05-03 12:38:19 +0100

    confuracoes do projeto

--- secao04/core/configs.py
+++ secao04/core/configs.py
@@ -4,16 +4,14 @@ from sqlalchemy.ext.declarative import declarative_base
 
 class Settings(BaseSettings):
     """
-    Configuracoes gerais usadas na aplicacao.
+    Configurações gerais usadas na aplicação
     """
     API_V1_STR: str = '/api/v1'
-    DB_URL: str = "postgresql+asyncpg://admin:password@localhost:5432/faculdade"
+    DB_URL: str = "postgresql+asyncpg://postgres:password@localhost:5432/faculdade"
     DBBaseModel = declarative_base()
-    
+
     class Config:
-        case_sensite = True
-         
+        case_sensitive = True
+
 
 settings = Settings()
-    
-    
--- secao04/core/deps.py
+++ secao04/core/deps.py
@@ -7,9 +7,8 @@ from core.database import Session
 
 async def get_session() -> Generator:
     session: AsyncSession = Session()
-    
+
     try:
         yield session
     finally:
         await session.close()
-        
